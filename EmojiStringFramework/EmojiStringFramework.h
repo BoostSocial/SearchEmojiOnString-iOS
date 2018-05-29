4a892f9bd56279655dceb984fe3a03f109d296fd
magicfeet <magicfeet55@gmail.com>
2018-05-28 22:18:14 -0600

    add carthage support

---
+++ EmojiStringFramework/EmojiStringFramework.h
@@ -0,0 +1,19 @@
+//
+//  EmojiStringFramework.h
+//  EmojiStringFramework
+//
+//  Created by Jeremy Smith on 5/28/18.
+//  Copyright © 2018 GabrielMassana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmojiStringFramework.
+FOUNDATION_EXPORT double EmojiStringFrameworkVersionNumber;
+
+//! Project version string for EmojiStringFramework.
+FOUNDATION_EXPORT const unsigned char EmojiStringFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmojiStringFramework/PublicHeader.h>
+
+#import "NSString+EMOEmoji.h"
